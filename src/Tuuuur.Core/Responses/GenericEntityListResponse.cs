5b2fb3aa68569e6eb25e4c6064c5ccf8624ac6e0
Florent Dubut <florentdubut@MacBook-Pro-de-Florent.local>
2025-09-20 09:11:07 +0200

    Fix UTs

--- src/Tuuuur.Core/Responses/GenericEntityListResponse.cs
+++ src/Tuuuur.Core/Responses/GenericEntityListResponse.cs
@@ -2,13 +2,13 @@ using Tuuuur.Domain.Bo;
 
 namespace Tuuuur.Core.Responses;
 
-public class GenericEntityListResponse<T> : UseCaseResponseMessage<IEnumerable<IBOEntity>> where T : IBOEntity
+public class GenericEntityListResponse<T> : UseCaseResponseMessage<IEnumerable<T>> where T : class, IBOEntity
 {
     public GenericEntityListResponse(IEnumerable<ErrorDto> p_Errors, string p_Message = null) : base(p_Errors, p_Message)
     {
     }
 
-    public GenericEntityListResponse(IEnumerable<IBOEntity> p_Value, string p_Message = null) : base(p_Value, p_Message)
+    public GenericEntityListResponse(IEnumerable<T> p_Value, string p_Message = null) : base(p_Value, p_Message)
     {
     }
 }
