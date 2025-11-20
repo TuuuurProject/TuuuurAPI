8059553b43dd8b1ae13f1421e3df79b71ade09b5
Florent <florent.dubut@gmail.com>
2025-11-20 17:06:16 +0100

    uts

--- src/Tuuuur.API/Requests/CodeRequest.cs
+++ src/Tuuuur.API/Requests/CodeRequest.cs
@@ -13,20 +13,3 @@ public record CodeRequest
     /// </summary>
     public string Code { get; set; }
 }
-/// <summary>
-/// Validator for code request
-/// </summary>
-public class CodeRequestValidator : AbstractValidator<CodeRequest>
-{
-    /// <summary>
-    /// ctor containing validation rules
-    /// </summary>
-    public CodeRequestValidator()
-    {
-        RuleFor(p_Request => p_Request.Code)
-            .NotEmpty()
-            .WithErrorCode(DomainErrors.Party.Code.Empty)
-            .Matches(@"^\d{6}$")
-            .WithErrorCode(DomainErrors.Party.Code.Invalid);
-    }
-}
