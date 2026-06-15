0b757f08ea29ffadc9f7240405c3d75ca42ab811
ElFloww <95478181+ElFloww@users.noreply.github.com>
2026-06-15 15:45:37 +0200

    Fix(): Ranked

--- src/Tuuuur.API/Hubs/RankedHub.cs
+++ src/Tuuuur.API/Hubs/RankedHub.cs
@@ -1,16 +1,14 @@
-using System.Security.Claims;
 using MediatR;
 using Microsoft.AspNetCore.SignalR;
-using Tuuuur.Core.Requests.Group;
 using Tuuuur.Core.Requests.Ranked;
+using Tuuuur.Domain.Configuration;
+using Tuuuur.Domain.Interfaces;
 using Tuuuur.Domain.Security;
-
 namespace Tuuuur.API.Hubs;
-
 /// <summary>
 /// SignalR notification hub for INotificationClient
 /// </summary>
-public class RankedHub(IMediator p_Mediator) : Hub<IRankedClient>
+public class RankedHub(IMediator p_Mediator, ICacheService p_CacheService) : Hub<IRankedClient>
 {
     /// <summary>
     /// Called by the client to join opponent searching
@@ -25,7 +23,6 @@ public class RankedHub(IMediator p_Mediator) : Hub<IRankedClient>
             {
                 Guid v_Guid = Guid.TryParse(Context.User.Claims.FirstOrDefault(p_Claim => p_Claim.Type == ClaimNames.Id)
                     ?.Value ?? string.Empty, out Guid v_UserId) ? v_UserId : Guid.Empty;
-
                 // Create party
                 JoinSearchOpponentRequest v_Request = new(v_Guid);
                 _ = await p_Mediator.Send(v_Request);
@@ -36,7 +33,6 @@ public class RankedHub(IMediator p_Mediator) : Hub<IRankedClient>
             await Clients.Caller.OnError(v_Exception.Message);
         }
     }
-
     /// <summary>
     /// Called by the client to leave opponent searching
     /// </summary>
@@ -50,7 +46,6 @@ public class RankedHub(IMediator p_Mediator) : Hub<IRankedClient>
             {
                 Guid v_Guid = Guid.TryParse(Context.User.Claims.FirstOrDefault(p_Claim => p_Claim.Type == ClaimNames.Id)
                     ?.Value ?? string.Empty, out Guid v_UserId) ? v_UserId : Guid.Empty;
-
                 // Create party
                 LeaveSeachOpponentRequest v_Request = new(v_Guid);
                 _ = await p_Mediator.Send(v_Request);
@@ -75,7 +70,6 @@ public class RankedHub(IMediator p_Mediator) : Hub<IRankedClient>
             {
                 Guid v_Guid = Guid.TryParse(Context.User.Claims.FirstOrDefault(p_Claim => p_Claim.Type == ClaimNames.Id)
                     ?.Value ?? string.Empty, out Guid v_UserId) ? v_UserId : Guid.Empty;
-
                 // Create party
                 AnswerQuestionRankedRequest v_AnswerQuestionGroupPartyRequest = new(p_AnswerId, v_Guid);
                 _ = await p_Mediator.Send(v_AnswerQuestionGroupPartyRequest);
@@ -101,7 +95,6 @@ public class RankedHub(IMediator p_Mediator) : Hub<IRankedClient>
             {
                 Guid v_Guid = Guid.TryParse(Context.User.Claims.FirstOrDefault(p_Claim => p_Claim.Type == ClaimNames.Id)
                     ?.Value ?? string.Empty, out Guid v_UserId) ? v_UserId : Guid.Empty;
-
                 // Create party
                 GiveUpRankedRequest v_Request = new(v_Guid);
                 _ = await p_Mediator.Send(v_Request);
@@ -111,5 +104,37 @@ public class RankedHub(IMediator p_Mediator) : Hub<IRankedClient>
         {
             await Clients.Caller.OnError(v_Exception.Message);
         }
+    }
+    /// <summary>
+    /// Called when a new connection is established with the hub.
+    /// </summary>
+    public override async Task OnConnectedAsync()
+    {
+        if (Context.User != null)
+        {
+            Guid v_Guid = Guid.TryParse(Context.User.Claims.FirstOrDefault(p_Claim => p_Claim.Type == ClaimNames.Id)
+                ?.Value ?? string.Empty, out Guid v_UserId) ? v_UserId : Guid.Empty;
+            if (v_Guid != Guid.Empty)
+            {
+                await p_CacheService.SetAsync(RedisKeys.User.UserConnected(v_Guid), true);
+            }
+        }
+        await base.OnConnectedAsync();
+    }
+    /// <summary>
+    /// Called when a connection with the hub is terminated.
+    /// </summary>
+    public override async Task OnDisconnectedAsync(Exception p_Exception)
+    {
+        if (Context.User != null)
+        {
+            Guid v_Guid = Guid.TryParse(Context.User.Claims.FirstOrDefault(p_Claim => p_Claim.Type == ClaimNames.Id)
+                ?.Value ?? string.Empty, out Guid v_UserId) ? v_UserId : Guid.Empty;
+            if (v_Guid != Guid.Empty)
+            {
+                await p_CacheService.SetAsync(RedisKeys.User.UserConnected(v_Guid), false);
+            }
+        }
+        await base.OnDisconnectedAsync(p_Exception);
     }
 }
